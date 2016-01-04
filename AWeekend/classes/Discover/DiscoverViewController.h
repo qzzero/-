e7c4115230236f3f3c304a15206cd6855670bb98
乔自朋 <1320280834@qq.com>
2016-01-04 15:05:12 +0800

    工程基本配置

---
+++ AWeekend/classes/Discover/DiscoverViewController.h
@@ -0,0 +1,13 @@
+//
+//  DiscoverViewController.h
+//  AWeekend
+//
+//  Created by scjy on 16/1/4.
+//  Copyright © 2016年 qzp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiscoverViewController : UIViewController
+
+@end
---
+++ AWeekend/classes/Main/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  AWeekend
+//
+//  Created by scjy on 16/1/4.
+//  Copyright © 2016年 qzp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
